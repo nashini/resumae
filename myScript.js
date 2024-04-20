bdc2a99b442c53ab8474647fa79fed3e5be4ce0c
nashini <146540103+nashini@users.noreply.github.com>
2024-04-20 01:16:39 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,26 @@
+function myFun(msg ){
+    let cusAlert=
+    
+    alert("Please enter your name no spcial characters are allowed");
+
+}
+function myFun1(){
+    
+    alert("Please enter your father  name or initial");
+
+}
+function myFun2(){
+    
+    alert("Please enter your correct email adrees only");
+
+}
+function myFun3(){
+    
+    alert("minimum lenth of password is 8 include a atleast one special character and numbers and Capital letter ");
+
+}
+function myFun4(){
+    
+    alert("Reter your password and confirm it");
+
+}
